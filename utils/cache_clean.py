e79d8850e375bfff82d7d5fe3eb1d00a695b2860
andrelrf1 <andre_al1@live.com>
2020-04-14 16:58:34 -0300

    [DONE] Classe de gerenciamento de cache; [START] Módulo de geração de números aleatórios.

---
+++ utils/cache_clean.py
@@ -0,0 +1,39 @@
+import os
+
+
+class CacheClean:
+    __cache_path = None
+
+    def __init__(self, cache_path: str):
+        self.cache_path = cache_path
+
+    @property
+    def cache_path(self):
+        return self.__cache_path
+
+    @cache_path.setter
+    def cache_path(self, path: str):
+        if not isinstance(path, str):
+            raise TypeError('O valor esperado é um str, não um int')
+
+        if not os.path.isdir(path):
+            raise FileNotFoundError(f'O sistema não encontrou o caminho especificado: "{path}"')
+
+        self.__cache_path = path
+
+    def cache_size(self):
+        size = len(os.listdir(self.__cache_path))
+        return size
+
+    def cache_list(self):
+        content_list = os.listdir(self.__cache_path)
+        return content_list
+
+    def cache_clean(self):
+        files = self.cache_list()
+        [os.remove(self.__cache_path + file) for file in files]
+
+
+if __name__ == '__main__':  # Para testes
+    teste = CacheClean('coisa')
+    print(teste.cache_path)
---
+++ utils/random_number.py
@@ -0,0 +1,17 @@
+import random
+
+
+def get_one_number():
+    pass
+
+
+def get_one_number_between(second_number: int, first_number: int = 0):
+    pass
+
+
+def get_one_number_list(repeat_numbers: bool, size: int):
+    pass
+
+
+def get_one_number_list_between(repeat_numbers: bool, size: int, second_number: int, first_number: int = 0):
+    pass
